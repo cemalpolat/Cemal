c70bf8aed57826ef146dac0a8c4133b9d079c65c
Hakan Gok <hakangok1974@yahoo.com>
2022-04-26 13:22:48 +0100

    2nd commit in Ayse's

--- src/Ayse.java
+++ src/Ayse.java
@@ -3,4 +3,9 @@ public class Ayse {
     //step1
     //step2
     //step3
+
+    // Ariane's change
+    public static void main(String[] args) {
+        System.out.println("Hi, Ayse. I'm in your class!");
+    }
 }
